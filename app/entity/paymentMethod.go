d98c000370f611f69e87c5b26442e031e2b16570
Peter James <peterjamesw@yahoo.com>
2021-10-11 07:54:41 +0700

    change payment method database

--- app/entity/paymentMethod.go
+++ app/entity/paymentMethod.go
@@ -20,9 +20,7 @@ type PaymentMethod struct {
 	BankCode 		 		string    		`gorm:"type:varchar(191) NULL default NULL;default:null" json:"bank_code"`
 	Name 		 			string    		`gorm:"type:varchar(191) NOT NULL" json:"name"`
 	PaymentLogo 		 	string    		`gorm:"type:TEXT NOT NULL" json:"payment_logo"`
-	RateApplied 		 	bool    		`gorm:"type:tinyint(1) NOT NULL" json:"rate_applied"`
-	TransactionFee 		 	int    			`gorm:"type:double(8,2) NOT NULL" json:"transaction_fee"`
-	PercentageFee 		 	int    			`gorm:"type:INT NOT NULL" json:"percentage_fee"`
+	// RateApplied 		 	bool    		`gorm:"type:tinyint(1) NOT NULL" json:"rate_applied"`
 	// UserId 		 			int    			`gorm:"type:INT UNSIGNED NOT NULL" json:"user_id"`
 	Status 		 			string    		`gorm:"type:enum('active','inactive') NOT NULL DEFAULT 'inactive';default:'inactive'" json:"status"`
 	Spec 		 			string    		`gorm:"type:LONGTEXT" json:"spec"`
--- app/entity/paymentMethodRate.go
+++ app/entity/paymentMethodRate.go
@@ -6,11 +6,12 @@ import (
 
 type PaymentMethodRate struct {
 	ID               		int            	`gorm:"type:BIGINT UNSIGNED NOT NULL AUTO_INCREMENT" json:"id"`
-	PaymentMethodId 		int	    	`gorm:"type:INT UNSIGNED NOT NULL" json:"payment_method_id"`
-	MinTransaction  		int    		`gorm:"type:INT UNSIGNED NOT NULL" json:"min_transaction"`
-	MaxTransaction  		int    		`gorm:"type:INT UNSIGNED NOT NULL" json:"max_transaction"`
-	TransactionFee  		int    		`gorm:"type:DOUBLE(8,2) NOT NULL" json:"transaction_fee"`
-	Premi  					int    		`gorm:"type:DOUBLE(8,2) NOT NULL" json:"premi"`
+	PaymentMethodCode 		string	    	`gorm:"type:VARCHAR(191) NOT NULL" json:"payment_method_code"`
+	MinTransaction  		int    			`gorm:"type:INT UNSIGNED NOT NULL" json:"min_transaction"`
+	MaxTransaction  		int    			`gorm:"type:INT UNSIGNED NOT NULL" json:"max_transaction"`
+	TransactionFee  		int    			`gorm:"type:DOUBLE(8,2) NOT NULL" json:"transaction_fee"`
+	PercentageFee 		 	int    			`gorm:"type:INT NOT NULL" json:"percentage_fee"`
+	Premi  					int    			`gorm:"type:DOUBLE(8,2) NOT NULL" json:"premi"`
 	CreatedAt        		time.Time      	`gorm:"type:TIMESTAMP DEFAULT CURRENT_TIMESTAMP" json:"created_at"`
 	UpdatedAt        		time.Time      	`gorm:"type:TIMESTAMP DEFAULT CURRENT_TIMESTAMP" json:"updated_at"`
 }
